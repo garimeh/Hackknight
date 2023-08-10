b221e7970371425d4e0a2513fa62bb4b52e9c464
garimeh <garimaminocha2@gmail.com>
2023-08-10 19:10:16 +0530

    extracted urls

--- datascrape.py
+++ datascrape.py
@@ -2,19 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# URL of the sitemap
 sitemap_url = 'https://www.investopedia.com/sitemap_1.xml'
 
-# Send a GET request to the sitemap
 response = requests.get(sitemap_url)
 
-# Parse the XML content
 soup = BeautifulSoup(response.content, 'lxml')
 
-# Find all 'loc' tags within 'url' tags using CSS selectors
 locs = soup.select('url > loc')
-
-# Extract and print the URLs
 
 with open('urls.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
